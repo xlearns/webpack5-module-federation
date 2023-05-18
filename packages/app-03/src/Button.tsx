f984acbdf4297d1d862538ef842121c7cddaf78d
xlearns <2447684095@qq.com>
2023-05-18 10:51:30 +0800

    fix: serve mode no file

--- packages/app-03/src/Button.tsx
+++ packages/app-03/src/Button.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
 
 function App() {
-  return <button>app03 - button</button>
+  return (
+    <button
+      onClick={() => {
+        console.log('app-03 click')
+      }}
+    >
+      app03 - button
+    </button>
+  )
 }
 
 export default App
